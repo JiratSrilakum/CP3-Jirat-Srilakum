5818f5370166d209e6a52e7845158a5a1f695384
unknown <boom.jirat@gmail.com>
2021-03-20 01:36:03 +0700

    Create Excercise4_Jirat_S.py

---
+++ Excercise4_Jirat_S.py
@@ -0,0 +1,10 @@
+FE = 80.3
+GB = 76.8
+ICS = 94.0
+Compro = 84.5
+print("--- Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",Compro)
+print("-----------------")
