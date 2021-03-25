643b378dc8fb0f7d763394a5d77e558ad7f276b8
JiratSrilakum <80918456+JiratSrilakum@users.noreply.github.com>
2021-03-26 01:38:16 +0700

    Add files via upload

---
+++ Assignments/Lecture50_Jirat_S.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def subtractNumber(x,y):
+    print(x-y)
+def mutiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(int(x/y))
+addNumber(5,2)
+subtractNumber(4,3)
+mutiplyNumber(10,2)
+divideNumber(20,2)
